e0d10740071bab5c8ef536155600cd8305149044
PuszAtiCkik <pusztai.attila@gmail.hu>
2024-05-07 22:35:42 +0200

    tizenharmadik

--- script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $("#myInput").on("keyup", function () {
-        var value = $(this).val().toLowerCase();
-        $("#myList li").each(function () {
-            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
-        });
-        $("#myList").toggle(value !== "");
-    });
-});
